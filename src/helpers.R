d07226e04329199c4afdd2aee07d52f4f43020f7
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2021-11-22 11:51:50 -0300

    Advance in analyzing dc dataset

--- src/helpers.R
+++ src/helpers.R
@@ -2,38 +2,49 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
-get_proportions_df <- function(seurat_object, fcoex_object, algorithm_name="fcoex") {
-  module_idents <- data.frame(fcoex_object@mod_idents)
-  colnames(module_idents) <- paste(algorithm_name, colnames(module_idents), sep="_")
-  
-  # Caution! cbind assumes order is kept the same, what is generally true,
-  # but might lead to silent errors if not.  
-  metadata = cbind(seurat_object@meta.data, module_idents)
-  
-  original_clusters <- data.frame(table(Idents(seurat_object)))
-  seurat_to_fcoex_proportions = list()
-  
-  for (identity in levels(Idents(seurat_object))){
+get_proportions_df <-
+  function(seurat_object,
+           fcoex_object,
+           algorithm_name = "fcoex",
+           ident_column = "seurat_annotations") {
+    module_idents <- data.frame(fcoex_object@mod_idents)
+    colnames(module_idents) <-
+      paste(algorithm_name, colnames(module_idents), sep = "_")
     
-    identity_metadata = metadata[metadata["seurat_annotations"] == identity,]
+    # Caution! cbind assumes order is kept the same, what is generally true,
+    # but might lead to silent errors if not.
+    metadata = cbind(seurat_object@meta.data, module_idents)
     
-    proportions_for_this_identity = list()
+    metadata <- droplevels(metadata)
     
-    for (col_name in colnames(identity_metadata)){
-      if (grepl(algorithm_name, col_name)){
-        total_cells = nrow(identity_metadata)
-        header_positive_cells = sum(identity_metadata[col_name] == "HP")
-        proportions_for_this_identity[[col_name]] <- header_positive_cells/total_cells 
-      }
-      seurat_to_fcoex_proportions[[identity]] <- proportions_for_this_identity
-    }
-  }
-  
-  proportions_df <- data.frame(do.call(rbind, seurat_to_fcoex_proportions))
-  return(proportions_df)
-}
+    
+    original_clusters <- data.frame(table(metadata[ident_column]))
+    seurat_to_fcoex_proportions = list()
+    
+    for (identity in levels(metadata[,ident_column])) {
 
-prepare_proportions_df <-function(proportions_df){
+      identity_metadata = metadata[metadata[ident_column] == identity, ]
+      
+      proportions_for_this_identity = list()
+      
+      for (col_name in colnames(identity_metadata)) {
+        if (grepl(algorithm_name, col_name)) {
+          total_cells = nrow(identity_metadata)
+          header_positive_cells = sum(identity_metadata[col_name] == "HP")
+          proportions_for_this_identity[[col_name]] <-
+            header_positive_cells / total_cells
+        }
+        seurat_to_fcoex_proportions[[identity]] <-
+          proportions_for_this_identity
+      }
+    }
+    
+    proportions_df <-
+      data.frame(do.call(rbind, seurat_to_fcoex_proportions))
+    return(proportions_df)
+  }
+
+prepare_proportions_df <- function(proportions_df, change_level_order_for_plotting = TRUE) {
   # Cluster to get factor order ----------
   seurat_distances <- dist(proportions_df)
   fcoex_distances <- dist(t(proportions_df))
@@ -43,26 +54,32 @@ prepare_proportions_df <-function(proportions_df){
   # Pivot to longer format -----------
   proportions_df$Seurat <- rownames(proportions_df)
   proportions_df_long <- proportions_df %>%
-    pivot_longer(cols = !Seurat,names_to="fcoex", values_to="proportion") 
+    pivot_longer(cols = !Seurat,
+                 names_to = "fcoex",
+                 values_to = "proportion")
   
+  proportions_df_long$proportion <-
+    as.numeric(proportions_df_long$proportion)
+  if (change_level_order_for_plotting){
   # Adjust types and order of levels of columns in dataframe ----------
-  proportions_df_long$proportion <- as.numeric(proportions_df_long$proportion)
-  
-  proportions_df_long$Seurat <- factor(proportions_df_long$Seurat, levels = seurat_clustering$labels[seurat_clustering$order])
-  
-  proportions_df_long$fcoex <- factor(proportions_df_long$fcoex, levels = fcoex_clustering$labels[fcoex_clustering$order])
-  
+
+    proportions_df_long$Seurat <-
+      factor(proportions_df_long$Seurat, levels = seurat_clustering$labels[seurat_clustering$order])
+    
+    proportions_df_long$fcoex <-
+      factor(proportions_df_long$fcoex, levels = fcoex_clustering$labels[fcoex_clustering$order])
+  }
   proportions_df_long
   
 }
 
-plot_proportions_df <- function(proportions_df){
+plot_proportions_df <- function(proportions_df, change_level_order_for_plotting = TRUE) {
   proportions_df %>%
-    prepare_proportions_df() %>%
-    ggplot(aes_string(x="Seurat", y="fcoex", fill="proportion")) +
+    prepare_proportions_df(change_level_order_for_plotting) %>%
+    ggplot(aes_string(x = "Seurat", y = "fcoex", fill = "proportion")) +
     geom_tile() +
     scale_fill_gradient(low = "white", high = "purple3") +
-    theme_bw()+
+    theme_bw() +
     theme(axis.text.x = element_text(angle = 70, hjust = 1))
   
 }
--- src/run_antigen_presenting_cells.R
+++ src/run_antigen_presenting_cells.R
@@ -1,28 +1,62 @@
 library(Seurat)
 library(fcoex)
+library(readr)
+source("src/helpers.R")
 
-
-filename <- "/home/lubianat/Documents/lab_related/fcoex_analysis/data/apc_covid/GSE169346_ValidationSet_RPMI_SeuratObj.Rds"
+filename <-
+  "/home/lubianat/Documents/lab_related/fcoex_analysis/data/apc_covid/GSE169346_ValidationSet_RPMI_SeuratObj.Rds"
 apc_validation <- readRDS(filename)
 
-colnames(apc_validation@meta.data)
-Seurat::DimPlot(apc_validation, group.by = "SCT_snn_res.1.2" )
+# Run fcoex for healthy subset
+healthy <-
+  subset(x = apc_validation, subset = severity == "Healthy")
+rm(apc_validation)
+gc()
 
-exprs <- as.data.frame(Seurat::GetAssayData(object = apc_validation, slot = "data"))
-exprs <- exprs[1:10000, 1:10000]
-target < apc_validation@meta.data$SCT_snn_res.1.2[1:10000]
+# Sample for local processing
+healthy_subsample <-
+  healthy[, sample(colnames(healthy), size = 1000, replace = F)]
+
+exprs <-
+  as.data.frame(Seurat::GetAssayData(object = healthy_subsample, slot = "data"))
+target <- healthy_subsample@meta.data$celltype
+
+fc <- new_fcoex(data.frame(exprs), target)
+fc <- discretize(fc)
+fc <-
+  find_cbf_modules(
+    fc,
+    verbose = TRUE,
+    n_genes_selected_in_first_step = 100,
+    is_parallel = TRUE
+  )
+fc <- recluster(fc)
+
+write_rds(x = fc,
+          file = "./data/intermediate_data/apc_healthy_small.rds",
+          compress = c("gz"))
 
 
-fc <- new_fcoex(data.frame(exprs),target)
+proportions_df = get_proportions_df(seurat_object = healthy_subsample,
+                   fcoex_object = fc ,
+                   ident_column = "celltype",
+                   algorithm_name = "fcoex")
+
+plot_proportions_df(proportions_df, change_level_order_for_plotting = FALSE)
+
+# Process fcoex for severe cases
+apc_validation <- readRDS(filename)
+severe <- subset(x = apc_validation, subset = severity == "Severe")
+  
+  
+  
+fc <- new_fcoex(data.frame(exprs), target)
 fc <- discretize(fc)
 
 fc <- find_cbf_modules(fc)
 fc <- recluster(fc)
-
-write_rds(
-  x=fc,
-  file="./data/intermediate_data/prototype_apc_validation.rds",
-  compress = c("gz"))
-
-
-
+  
+write_rds(x = fc,
+            file = "./data/intermediate_data/apc_severe_covid.rds",
+            compress = c("gz"))
+  
